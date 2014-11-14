443e56a3018c9309d8d595618171f633f33fd261
Ömer Emre Aslan <omeremreaslan@Omers-MacBook-Pro.local>
2014-11-14 21:56:16 +0200

    Initial Commit

---
+++ dizitakip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dizitakip
+//
+//  Created by Ömer Emre Aslan on 14/11/14.
+//  Copyright (c) 2014 OEASLAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
