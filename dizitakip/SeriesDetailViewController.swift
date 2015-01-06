9a94be10969761f07e1df176d51831184a8774ec
Ömer Aslan <omer.aslan@ozu.edu.tr>
2015-01-06 23:42:47 +0200

    Added Series Detail Page

---
+++ dizitakip/SeriesDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SeriesDetailViewController.swift
+//  dizitakip
+//
+//  Created by Ömer Emre Aslan on 06/01/15.
+//  Copyright (c) 2015 OEASLAN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SeriesDetailViewController:UIViewController{
+    @IBOutlet weak var seriesImage: UIImageView!
+    @IBOutlet weak var seriesTitle: UILabel!
+    override func viewDidLoad() {
+        
+    }
+}
