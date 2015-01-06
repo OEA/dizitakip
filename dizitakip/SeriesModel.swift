7343af219f48fca77b4fa2392ff9083985009a43
Ömer Aslan <omer.aslan@ozu.edu.tr>
2015-01-06 23:46:35 +0200

    Added series functions

--- dizitakip/SeriesModel.swift
+++ dizitakip/SeriesModel.swift
@@ -14,7 +14,55 @@ class SeriesModel{
     let apiSecret = "268403caf178514c213cc9ee41bf7252848a4b50"
     var apiUrl = "http://localhost/imdb/api/"
     
-    var series: [String:String]! = nil
+    var top10: [[String:String]]! = nil
+    var recent: [[String:String]]! = nil
     var errorMessage: String! = nil
     
+    func setTop10(){
+        apiUrl = apiUrl + "getTop10?apiKey=" + apiKey + "&apiSecret=" + apiSecret
+        
+        if let nsurl = NSURL(string: apiUrl) {
+            
+            if let nsdata = NSData(contentsOfURL: nsurl) {
+                
+                var jsonDict: [String:AnyObject]!
+                jsonDict = NSJSONSerialization.JSONObjectWithData(nsdata, options: NSJSONReadingOptions.AllowFragments, error: nil) as [String:AnyObject]
+                
+                var status = jsonDict["status"] as String
+                
+                
+                if (status=="success" as String){
+                    top10 = jsonDict["result"] as [[String:String]]
+                }else{
+                    errorMessage = jsonDict["error_message"] as String
+                }
+                
+                
+            }
+        }
+    }
+    func setRecent(){
+        apiUrl = apiUrl + "recent?apiKey=" + apiKey + "&apiSecret=" + apiSecret
+        
+        if let nsurl = NSURL(string: apiUrl) {
+            
+            if let nsdata = NSData(contentsOfURL: nsurl) {
+                
+                var jsonDict: [String:AnyObject]!
+                jsonDict = NSJSONSerialization.JSONObjectWithData(nsdata, options: NSJSONReadingOptions.AllowFragments, error: nil) as [String:AnyObject]
+                
+                var status = jsonDict["status"] as String
+                
+                
+                if (status=="success" as String){
+                    top10 = jsonDict["result"] as [[String:String]]
+                }else{
+                    errorMessage = jsonDict["error_message"] as String
+                }
+                
+                
+            }
+        }
+    }
+
 }
