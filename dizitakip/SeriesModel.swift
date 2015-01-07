4ee783d0104d1028806c4185078b1c16073c7598
Ömer Aslan <omer.aslan@ozu.edu.tr>
2015-01-07 23:48:46 +0200

    Added necessary methods

--- dizitakip/SeriesModel.swift
+++ dizitakip/SeriesModel.swift
@@ -12,15 +12,17 @@ class SeriesModel{
    
     let apiKey = "d3ff368943be4b576251437784234c5d3a200160"
     let apiSecret = "268403caf178514c213cc9ee41bf7252848a4b50"
-    var apiUrl = "http://localhost/imdb/api/"
-    
+    let baseApiUrl = "http://localhost/imdb/api/"
+    var runUrl: String! = nil
     var top10: [[String:String]]! = nil
     var recent: [[String:String]]! = nil
+    var favorites: [[String:String]]! = nil
     var errorMessage: String! = nil
+    var likedOrNot: String! = nil
     
     func setTop10(){
-        apiUrl = apiUrl + "getTop10?apiKey=" + apiKey + "&apiSecret=" + apiSecret
-        
+        var apiUrl = baseApiUrl + "getTop10?apiKey=" + apiKey + "&apiSecret=" + apiSecret
+        runUrl = apiUrl
         if let nsurl = NSURL(string: apiUrl) {
             
             if let nsdata = NSData(contentsOfURL: nsurl) {
@@ -41,9 +43,11 @@ class SeriesModel{
             }
         }
     }
-    func setRecent(){
-        apiUrl = apiUrl + "recent?apiKey=" + apiKey + "&apiSecret=" + apiSecret
+    func setFavorites(userId: Int){
         
+        var strUID: String = String(userId)
+        var apiUrl = baseApiUrl + "likedseries?apiKey=" + apiKey + "&apiSecret=" + apiSecret + "&userId=" + strUID
+        runUrl = apiUrl
         if let nsurl = NSURL(string: apiUrl) {
             
             if let nsdata = NSData(contentsOfURL: nsurl) {
@@ -55,7 +59,7 @@ class SeriesModel{
                 
                 
                 if (status=="success" as String){
-                    top10 = jsonDict["result"] as [[String:String]]
+                    favorites = jsonDict["result"] as [[String:String]]
                 }else{
                     errorMessage = jsonDict["error_message"] as String
                 }
@@ -64,5 +68,58 @@ class SeriesModel{
             }
         }
     }
-
+    
+    func likeSeries(seriesId:Int , userId: Int){
+        var strSID: String = String(seriesId)
+        var strUID: String = String(userId)
+        
+        var apiUrl = baseApiUrl + "likeSeries?apiKey=" + apiKey + "&apiSecret=" + apiSecret + "&seriesId="+strSID+"&userId=" + strUID
+        runUrl = apiUrl
+        if let nsurl = NSURL(string: apiUrl) {
+            if let nsdata = NSData(contentsOfURL: nsurl) {
+                
+                println(apiUrl)
+                var jsonDict: [String:AnyObject]!
+                jsonDict = NSJSONSerialization.JSONObjectWithData(nsdata, options: NSJSONReadingOptions.AllowFragments, error: nil) as [String:AnyObject]
+                
+                var status = jsonDict["status"] as String
+                
+                
+                if (status=="success" as String){
+                    likedOrNot = jsonDict["message"] as String
+                }else{
+                    errorMessage = jsonDict["message"] as String
+                }
+                
+                
+            }
+        }
+    }
+    
+    func isLikedSeries(seriesId: Int, userId:Int) -> Bool{
+        var strSID: String = String(seriesId)
+        var strUID: String = String(userId)
+        
+        var apiUrl = baseApiUrl + "isLikedSeries?apiKey=" + apiKey + "&apiSecret=" + apiSecret + "&seriesId="+strSID+"&userId=" + strUID
+        runUrl = apiUrl
+        if let nsurl = NSURL(string: apiUrl) {
+            if let nsdata = NSData(contentsOfURL: nsurl) {
+                
+                var jsonDict: [String:AnyObject]!
+                jsonDict = NSJSONSerialization.JSONObjectWithData(nsdata, options: NSJSONReadingOptions.AllowFragments, error: nil) as [String:AnyObject]
+                
+                var status = jsonDict["status"] as String
+                
+                
+                if (status=="success" as String){
+                    return true
+                }else{
+                    
+                }
+                
+            }
+        }
+        return false
+    }
+    
 }
