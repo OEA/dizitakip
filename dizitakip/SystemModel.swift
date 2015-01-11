a7739a5c02a7d0ef5044bcdf39dad51346457062
Ömer Aslan <omer.aslan@ozu.edu.tr>
2015-01-11 21:02:49 +0200

    Added statistics variable and method

--- dizitakip/SystemModel.swift
+++ dizitakip/SystemModel.swift
@@ -14,6 +14,7 @@ class SystemModel{
     
     var profile: [String:String]! = nil
     var errorMessage: String! = nil
+    var statistics: [String:String]! = nil
     func login(username: String, password: String) -> Bool{
         var apiUrl = baseApiUrl + "login?apiKey=" + apiKey + "&apiSecret=" + apiSecret + "&username=" + username + "&password=" + password
         
@@ -39,7 +40,30 @@ class SystemModel{
         
         return false
     }
-    
+    func setStatistics(user_id: String){
+        var apiUrl = baseApiUrl + "getstatistics?apiKey=" + apiKey + "&apiSecret=" + apiSecret + "&userId="+user_id
+        
+        if let nsurl = NSURL(string: apiUrl) {
+            
+            if let nsdata = NSData(contentsOfURL: nsurl) {
+                
+                var jsonDict: [String:AnyObject]!
+                jsonDict = NSJSONSerialization.JSONObjectWithData(nsdata, options: NSJSONReadingOptions.AllowFragments, error: nil) as [String:AnyObject]
+                
+                var status = jsonDict["status"] as String
+                
+                
+                if (status=="success" as String){
+                    var infos = jsonDict["result"] as [[String:String]]
+                    statistics = infos[0]
+                }else{
+                    errorMessage = jsonDict["error_message"] as String
+                }
+                
+                
+            }
+        }
+    }
     func register(username: String, password: String, email: String)->Bool{
         
         var apiUrl = baseApiUrl + "register?apiKey=" + apiKey + "&apiSecret=" + apiSecret + "&username=" + username + "&password=" + password + "&email=" + email
